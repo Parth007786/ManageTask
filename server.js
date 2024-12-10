c6bed50dac7c80d48899895e445ebed561363544
Parth007786 <parthmehtapimr1618@gmail.com>
2024-12-10 17:08:22 +0530

    chage uploads path

--- server.js
+++ server.js
@@ -26,9 +26,9 @@ app.use("/api/tasks", require("./backend/routes/taskRoute"));
 // Serve Static Frontend in Production
 
 let NODE_ENV = "production";
-if (true) {
+if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
-  app.use("/uploads", express.static("var/data/uploads"));
+  app.use("/uploads", express.static("/uploads"));
   app.use(express.static(path.join(__dirname, "frontend/dist")));
 
   app.get("*", (req, res) =>
