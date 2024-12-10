81e5306aee4ab5298f6f3a8457466656b74875ff
Parth007786 <parthmehtapimr1618@gmail.com>
2024-12-10 17:06:29 +0530

    server change

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use("/api/tasks", require("./backend/routes/taskRoute"));
 // Serve Static Frontend in Production
 
 let NODE_ENV = "production";
-if (process.env.NODE_ENV === "production") {
+if (true) {
   const __dirname = path.resolve();
   app.use("/uploads", express.static("var/data/uploads"));
   app.use(express.static(path.join(__dirname, "frontend/dist")));
