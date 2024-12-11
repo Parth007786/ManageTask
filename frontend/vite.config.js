874480f70ccfc802a27c051c423206e85db83105
Parth007786 <parthmehtapimr1618@gmail.com>
2024-12-11 15:56:06 +0530

    changed authservice

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://localhost:6565",
+        target: "https://managetask55.onrender.com/",
         changeOrigin: true,
         secure: false
       }
